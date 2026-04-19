b9c4c282dca4ab25a3d01e4fa0fcbdfbc296732c
Himangshu Dey <himangs503@gmail.com>
2026-04-19 20:53:40 +0530

    Initial commit: Setup Phase 0 and 1

---
+++ src/main/java/com/himangshu/Main.java
@@ -0,0 +1,14 @@
+package com.himangshu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--- SecureDrop-P2P Project Started ---");
+
+        // This is a test to ensure your environment is ready
+        System.out.println("Hello, Himangshu! Your Java environment is working.");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Initialising module " + i + "...");
+        }
+    }
+}
